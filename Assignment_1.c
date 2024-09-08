55632329961b9950a82c7422f0c16b427b7acb83
Braydon P <br686967@ucf.edu>
2024-09-08 17:40:49 +0000

    Correction, started writing code for a square or rectangle

--- Assignment_1.c
+++ Assignment_1.c
@@ -3,35 +3,23 @@
 
 #define PI 3.14159
 
-double calculateDistance();
-double calculatePerimeter();
-double calculateArea();
-double calculateWidth();
-double calculateHeight();
-double askForUserInput();
-
-int main(int argc, char **argv) {
-  calculateDistance();
-calculatePerimeter();
-calculateArea();
-calculateWidth();
-calculateHeight();
-return 0;
-}
-
+// Function to calculate the distance between two points (used as the diameter)
 double calculateDistance() {
-  double x1, y1, x2, y2;
-
-//getting cordinates from the user
-printf("Enter Coordinates for point #1 (x1 y1):");
-x1 = askForUserInput();
-y2 = askForUserInput();
-
-printf("Enter Coordinate for point #2 (x2 y2):");
-x2 = askForUserInput();
-y2 = askForUserInput();
-
-//Calulating the distance
-double distance = sqrt(pow(x2 - x1, 2) + pow(y2-y1, 2));
-
-//Output
+    double x1, y1, x2, y2;
+    
+    // Input for the coordinates of two points
+    printf("Enter coordinates for Point #1 (x1, y1): ");
+    scanf("%lf %lf", &x1, &y1);
+    printf("Enter coordinates for Point #2 (x2, y2): ");
+    scanf("%lf %lf", &x2, &y2);
+    
+    // Calculate distance (i.e., diameter of the circle)
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    
+    // Output the points and the distance
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The distance between the two points (diameter) is %.2f\n", distance);
+    
+    return distance;
+}
